72684425f1191b66c8ac496ee71a780740ef02dc
ZhanLuo <20109459@mail.wit.ie>
2025-05-19 12:18:55 +0100

    Initial Commit

---
+++ offlineSyncTodo/offlineSyncTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  offlineSyncTodoApp.swift
+//  offlineSyncTodo
+//
+//  Created by Luo HY on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct offlineSyncTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
